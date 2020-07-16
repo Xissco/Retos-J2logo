549d3ff33d214121d47b705469fc3bcd52054704
Xissco <52544926+Xissco@users.noreply.github.com>
2020-07-16 12:13:30 -0500

    Create cuadrado.py

---
+++ cuadrado.py
@@ -0,0 +1,18 @@
+def cuadrado(n): 
+  if n==0: print ([]) 
+  else: 
+    for fila in [[n]*n]*n: print(fila) 
+
+cuadrado(0)
+print('------')
+cuadrado(1)
+print('------')
+cuadrado(2)
+print('------')
+cuadrado(3)
+print('------')
+cuadrado(4)
+print('------')
+cuadrado(5)
+print('------')
+cuadrado(6)
